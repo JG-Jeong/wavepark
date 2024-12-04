01333a68d13860cd6beb0d5551fe197a5a879d7e
JG-Jeong <jo128g@naver.com>
2024-12-04 21:59:15 +0900

    title arrangement - 아직도 조금 어색한 부분이 있는데 조율해야 할듯.

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -5,12 +5,16 @@ import logo from "../../logo/WAVEPARK logo_color.png";
 const Header: React.FC = () => {
   // 현재 날짜 가져오기
   const today = new Date();
-  const formattedDate = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
+  const formattedDate = `${today.getFullYear()}-${String(
+    today.getMonth() + 1
+  ).padStart(2, "0")}-${String(today.getDate()).padStart(2, "0")}`;
 
   return (
     <div className={styles.header}>
-      <h1 className={styles.title}>웨이브파크 수온 알리미</h1>
-        <img src={logo} alt="Wavepark Logo" className={styles.logo}/>
+      <div className={styles.contentWrapper}>
+        <h1 className={styles.title}>웨이브파크 수온 알리미</h1>
+        <img src={logo} alt="Wavepark Logo" className={styles.logo} />
+      </div>
       <p className={styles.date}>{formattedDate}</p>
     </div>
   );
